e559e60314af5df0ca77196824f59782d9403635
shambhavishirgurkar <81633209+shambhavishirgurkar@users.noreply.github.com>
2025-01-17 12:11:25 +0530

    Add files via upload

---
+++ task1/SearchRecipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchRecipes = () => {
+  return (
+    <div>
+      <h2>Search Recipes</h2>
+      <p>Search for recipes using an API</p>
+    </div>
+  );
+};
+
+export default SearchRecipes;
---
+++ task1/ViewRecipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ViewRecipes = () => {
+  return (
+    <div>
+      <h2>View Recipes</h2>
+      <p>No recipes available</p>
+    </div>
+  );
+};
+
+export default ViewRecipes;
